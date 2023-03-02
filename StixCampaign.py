8c42f1a320d9a56ef28160aba5d08b834c5dcf31
wctrocky <wctrocky@gmail.com>
2023-03-02 11:57:46 -0500

    Added Campaign

--- StixCampaign.py
+++ StixCampaign.py
@@ -26,14 +26,23 @@ identity1 = Identity(
 attack_pattern1 = AttackPattern(
     type="attack-pattern",
     spec_version="2.1",
-    created="2016-08-08T15:50:10.983Z",
-    modified="2017-01-30T21:15:04.127Z",
+    created="2023-03-01T14:00:00.983Z",
     name="DDOS attack"
 )
+campaign1 = Campaign(
+    type="campaign",
+    spec_version="2.1",
+    created="2023-03-01T14:00:00.983Z",
+    name="Operation disguntled student",
+    description="A angry student tries to take down the wssu infastructure.",
+    aliases=["DSW"],
+    objective="shut down OneUp & WebAssign"
+)
+relationship3 = Relationship(campaign1, 'attributed-to', threat_actor)
 relationship1 = Relationship(threat_actor, 'targets', identity1)
 relationship2 = Relationship(threat_actor, 'uses', attack_pattern1)
 
-bundle = Bundle(objects=[threat_actor, identity1,attack_pattern1, relationship1, relationship2])
+bundle = Bundle(objects=[campaign1,threat_actor, identity1,attack_pattern1, relationship1, relationship2])
 #print(bundle.serialize(pretty=True))
 
 with open('bundle.json', 'w') as jsonFile:
