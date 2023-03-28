d31840e6044c121ac3c9d9b07ed6a63b8480e77d
wctrocky <wctrocky@gmail.com>
2023-03-28 11:49:09 -0400

    Added user input to RandomStix

--- RandomStix.py
+++ RandomStix.py
@@ -2,33 +2,49 @@ from stix2 import ThreatActor, Malware, Campaign, Identity, Relationship, Bundle
 from random_word import RandomWords
 
 totalObjects = 3
+totalCampaign = 3
+totalMalware = 3
+totalThreat = 3
+totalRelationship = 3
 objects = []
+campaignArray = []
+malwareArray = []
+threatArray = []
 
-for i in range(totalObjects):
+for i in range(totalCampaign):
     # Creating Campaign object
     objtype = "campaign"
     name = RandomWords().get_random_word() + " " + objtype
     campaign = Campaign(type=objtype,name=name)
+    campaignArray.extend([campaign])
+    objects.extend([campaign])
 
+for i in range(totalMalware):
     # Creating Malware object
     objtype = "malware"
     labels = "ddos"
     name = RandomWords().get_random_word() + " " + labels
     malware = Malware(type=objtype, labels=labels, name=name, is_family=False)
+    malwareArray.extend([malware])
+    objects.extend([malware])
 
+for i in range(totalThreat):
     # Creating ThreatActor object
     objtype = "threat-actor"
     name = RandomWords().get_random_word() + " " + objtype
     threat_actor = ThreatActor(type=objtype, name=name)
+    threatArray.extend([threat_actor])
+    objects.extend([threat_actor])
 
+for i in range(totalRelationship):
     # Creating Relationship object
     objtype = "relationship"
     relationship_type = "uses"
     relationship_type2 = "executes"
-    relationship = Relationship(type=objtype, relationship_type=relationship_type, source_ref=threat_actor.id, target_ref=malware.id)
-    relationship2 = Relationship(type=objtype, relationship_type=relationship_type2,source_ref=threat_actor.id, target_ref=campaign.id)
+    relationship = Relationship(type=objtype, relationship_type=relationship_type, source_ref=campaignArray[i], target_ref=malwareArray[i])
+    relationship2 = Relationship(type=objtype, relationship_type=relationship_type2,source_ref=threatArray[i], target_ref=campaignArray[i])
     # Adding objects to the objects list
-    objects.extend([campaign, malware, threat_actor, relationship,relationship2])
+    objects.extend([relationship,relationship2])
 
 # Creating the bundle object
 bundle = Bundle(objects=objects)
