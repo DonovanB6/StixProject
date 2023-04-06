59bf182d9baa98c0f866c9bf6c6f2f1bbd6df35b
wctrocky <wctrocky@gmail.com>
2023-04-06 11:29:53 -0400

    Split, does not evenly distribute yet

--- RandomStix.py
+++ RandomStix.py
@@ -3,8 +3,8 @@ from random_word import RandomWords
 
 totalObjects = 3
 totalCampaign = 3
-totalMalware = 3
-totalThreat = 1
+totalMalware = 5
+totalThreat = 2
 totalRelationship = 3
 objects = []
 campaignArray = []
@@ -37,28 +37,70 @@ for i in range(totalThreat):
     objects.extend([threat_actor])
 
 
-if len(threatArray) == 1:
-    for i in range(totalRelationship):
-    # Creating Relationship object
+# Calculate the number of malwares each threat actor will receive
+malwares_per_threat_actor = totalRelationship // len(threatArray)
+
+# Distribute malware evenly among threat actors
+# Calculate the number of malwares each threat actor will receive
+malwares_per_threat_actor = totalRelationship // len(threatArray)
+
+# Calculate the number of extra malwares to be assigned to the first threat actor
+extra_malwares = totalRelationship % len(threatArray)
+
+# Distribute malware evenly among threat actors
+for i in range(len(threatArray)):
+    malwares_for_this_actor = malwares_per_threat_actor
+    if i == 0:
+        malwares_for_this_actor += extra_malwares
+
+# Calculate the number of malwares each campaign will receive
+malwares_per_campaign = totalRelationship // len(campaignArray)
+
+# Calculate the number of extra malwares to be assigned to the first campaign
+extra_malwares = totalRelationship % len(campaignArray)
+
+# Distribute malware evenly among campaigns
+# Iterate through campaigns
+# Calculate the number of malwares each campaign will receive
+malwares_per_campaign = len(malwareArray) // len(campaignArray)
+
+# Calculate the number of extra malwares to be assigned to the first campaign
+extra_malwares = len(malwareArray) % len(campaignArray)
+
+# Initialize malware index
+malware_index = 0
+
+# Distribute malware evenly among campaigns
+for i in range(len(campaignArray)):
+    malwares_for_this_campaign = malwares_per_campaign
+    if i == 0:
+        malwares_for_this_campaign += extra_malwares
+
+    for j in range(malwares_for_this_campaign):
+        # Creating Relationship objects
         objtype = "relationship"
         relationship_type = "uses"
-        relationship_type2 = "executes"
-        relationship = Relationship(type=objtype, relationship_type=relationship_type, source_ref=campaignArray[i], target_ref=malwareArray[i])
-        print ("test")
-        relationship2 = Relationship(type=objtype, relationship_type=relationship_type2,source_ref=threatArray[0], target_ref=campaignArray[i])
-        objects.extend([relationship,relationship2])
-if len(threatArray) > 1:
-    for i in range(totalRelationship):
-    # Creating Relationship object
-        objtype = "relationship"
-        relationship_type = "uses"
-        relationship_type2 = "executes"
-        relationship = Relationship(type=objtype, relationship_type=relationship_type, source_ref=campaignArray[i], target_ref=malwareArray[i])
-        print ("test2")
-        relationship2 = Relationship(type=objtype, relationship_type=relationship_type2,source_ref=threatArray[i], target_ref=campaignArray[i])
+        relationship = Relationship(type=objtype, relationship_type=relationship_type, source_ref=campaignArray[i], target_ref=malwareArray[malware_index])
+
+        # Adding objects to the objects list
+        objects.append(relationship)
+
+        # Increment malware index
+        malware_index += 1
+
+    # Create a relationship between each threat actor and campaign
+    relationship_type2 = "executes"
+    relationship2 = Relationship(type=objtype, relationship_type=relationship_type2, source_ref=threatArray[i % len(threatArray)], target_ref=campaignArray[i])
+
     # Adding objects to the objects list
-        objects.extend([relationship,relationship2])
+    objects.append(relationship2)
+
 # Creating the bundle object
+
+# Creating the bundle object
+
+
+
 bundle = Bundle(objects=objects)
 
 # Serializing and printing the bundle object
